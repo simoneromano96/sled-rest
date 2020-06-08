74ff1df417ca116e668b05327d8864730294241d
Simone Romano <simoneromano@tutanota.de>
2020-06-08 22:29:48 +0200

    Update Cargo.toml and main.rs CRUD

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    error, web, App, Error as HttpError, HttpRequest, HttpResponse, HttpServer, Responder,
+    Result as HttpResult,
+};
 use bytes::Bytes;
 use sled::{Config, Db, Result};
 use std::sync::Arc;
@@ -7,6 +10,8 @@ struct AppData {
     db: Db,
 }
 
+// Just for reference
+/*
 fn basic(db: Db) -> Result<()> {
     let k = b"k".to_vec();
     let v1 = b"v1".to_vec();
@@ -37,6 +42,7 @@ fn basic(db: Db) -> Result<()> {
 
     Ok(())
 }
+*/
 
 fn init_db() -> Result<Db> {
     let config: Config = Config::default()
@@ -47,29 +53,58 @@ fn init_db() -> Result<Db> {
 }
 
 #[actix_web::put("/{key}")]
-async fn create(
+async fn create_or_update_value(
     key: web::Path<String>,
     data: web::Data<Arc<AppData>>,
     body: Bytes,
 ) -> impl Responder {
+    // Get a reference to our "DB"
     let db: Db = data.db.clone();
+    // From Bytes to bytes, duh
     let value: &[u8] = body.as_ref();
-    // let v1 = b"v1".to_vec();
-
+    // Simply put stuff into DB, this could be anything and there is no need to deserialize it
     db.insert(key.clone(), value).unwrap();
+    // Send back the body for confirmation
     HttpResponse::Ok().body(body)
 }
 
 #[actix_web::get("/{key}")]
-async fn get_key(key: web::Path<String>, data: web::Data<Arc<AppData>>) -> impl Responder {
+async fn read_value(key: web::Path<String>, data: web::Data<Arc<AppData>>) -> impl Responder {
     let db: Db = data.db.clone();
+    // Key from String to bytes
     let key_vec = key.clone().into_bytes();
+    // Get result
     let result = db.get(key_vec).unwrap();
 
+    // If has result
     if let Some(value) = result {
-        // let deserialised: String = std::str::from_utf8(&*value).unwrap().to_string();
-        HttpResponse::Ok().body(format!("{:?}", std::str::from_utf8(&*value).unwrap()))
+        // Get that thing as bytes vec (HttpResponse wants ownership of the var)
+        // This will allocate some memory but at least it will avoid serialization and deserialization
+        // Also why should I deserialize stuff from my DB? It must be clean (should be?)
+        let response = value.to_vec();
+        // Send it back as that thing
+        HttpResponse::Ok().body(response)
     } else {
+        // Not found
+        HttpResponse::NotFound().body(format!("Key {:?} not found", key))
+    }
+}
+
+#[actix_web::delete("/{key}")]
+async fn delete_value(key: web::Path<String>, data: web::Data<Arc<AppData>>) -> impl Responder {
+    let db: Db = data.db.clone();
+    // Key from String to bytes
+    let key_vec = key.clone().into_bytes();
+    // Get result
+    let result = db.remove(key_vec).unwrap();
+    // If has result
+    if let Some(value) = result {
+        // Get that thing as bytes vec (HttpResponse wants ownership of the var)
+        let response = value.to_vec();
+        // Send it back as that thing
+        HttpResponse::Ok().body(response)
+    } else {
+        // Not found
         HttpResponse::NotFound().body(format!("Key {:?} not found", key))
     }
 }
@@ -83,11 +118,13 @@ async fn main() -> std::io::Result<()> {
         db: init_db().unwrap(),
     });
 
+    // TODO: I should only accept JSON or else users will be able to send any kind of shit
     HttpServer::new(move || {
         App::new()
             .data(data.clone())
-            .service(get_key)
-            .service(create)
+            .service(create_or_update_value)
+            .service(read_value)
+            .service(delete_value)
     })
     .bind("127.0.0.1:8088")?
     .run()
